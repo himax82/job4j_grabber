b9036f0e579044c0555a46ad2270f195b0d8caf3
himax82 <himax82@gmail.com>
2021-09-24 01:23:17 +0800

    2. Парсинг HTML страницы. [#260358]

---
+++ src/html/SqlRuParse.java
@@ -0,0 +1,20 @@
+package html;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class SqlRuParse {
+    public static void main(String[] args) throws Exception {
+        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
+        Elements row = doc.select(".postslisttopic");
+        for (Element td : row) {
+            Element href = td.child(0);
+            System.out.println(href.attr("href"));
+            System.out.println(href.text());
+            Element date = td.parent();
+            System.out.println(date.children().get(5).text());
+        }
+    }
+}
