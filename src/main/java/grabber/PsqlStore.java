ab64dbee252a065031cb62cd59c99b1fc8cff67f
himax82 <himax82@gmail.com>
2021-09-24 18:34:54 +0800

    5. PsqlStore [#285209]

---
+++ src/main/java/grabber/PsqlStore.java
@@ -0,0 +1,110 @@
+package grabber;
+
+import grabber.utils.SqlRuDateTimeParser;
+import html.Post;
+import html.SqlRuParse;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+public class PsqlStore implements Store, AutoCloseable {
+
+    private Connection cnn;
+
+    public PsqlStore(Properties cfg) throws SQLException {
+        try {
+            Class.forName(cfg.getProperty("driver-class-name"));
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
+        cnn = DriverManager.getConnection(
+                cfg.getProperty("url"),
+                cfg.getProperty("username"),
+                cfg.getProperty("password")
+        );
+    }
+
+    @Override
+    public void save(Post post) {
+        try (PreparedStatement statement =
+                     cnn.prepareStatement("insert into post(name, text, link, created) values (?, ?, ?, ?)")) {
+            statement.setString(1, post.getTitle());
+            statement.setString(2, post.getDescription());
+            statement.setString(3, post.getLink());
+            statement.setTimestamp(4, Timestamp.valueOf(post.getCreated()));
+            statement.execute();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public List<Post> getAll() {
+        List<Post> posts = new ArrayList<>();
+        try (PreparedStatement statement = cnn.prepareStatement("select * from post")) {
+            try (ResultSet resultSet = statement.executeQuery()) {
+                while (resultSet.next()) {
+                    posts.add(new Post(
+                            resultSet.getInt("id"),
+                            resultSet.getString("name"),
+                            resultSet.getString("text"),
+                            resultSet.getString("link"),
+                            resultSet.getTimestamp("created").toLocalDateTime()
+                    ));
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return posts;
+    }
+
+    @Override
+    public Post findById(int id) {
+        Post post = null;
+        try (PreparedStatement statement =
+                     cnn.prepareStatement("select * from post where id = ?")) {
+            statement.setInt(1, id);
+            statement.execute();
+            try (ResultSet resultSet = statement.executeQuery()) {
+                if (resultSet.next()) {
+                    post = new Post(
+                            resultSet.getInt("id"),
+                            resultSet.getString("name"),
+                            resultSet.getString("text"),
+                            resultSet.getString("link"),
+                            resultSet.getTimestamp("created").toLocalDateTime()
+                    );
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return post;
+    }
+
+    @Override
+    public void close() throws Exception {
+        if (cnn != null) {
+            cnn.close();
+        }
+    }
+
+    public static void main(String[] args) {
+        try (InputStream in = PsqlStore.class.getClassLoader()
+                .getResourceAsStream("app.properties")) {
+            Properties config = new Properties();
+            config.load(in);
+            PsqlStore psqlStore = new PsqlStore(config);
+            SqlRuParse sqlRuParse = new SqlRuParse(new SqlRuDateTimeParser());
+            String link = "https://www.sql.ru/forum/1338576/spb-programmist-sql-ot130tys-na-ruki";
+            psqlStore.save(sqlRuParse.detail(link));
+        } catch (IOException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
