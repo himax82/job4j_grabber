afb551b9cdfed75cdd21936fb4eb4b2a90d55787
himax82 <himax82@gmail.com>
2021-09-24 18:39:25 +0800

    5. PsqlStore [#285209]

--- test/java/grabber/utils/SqlRuDateTimeParserTest.java
+++ test/java/grabber/utils/SqlRuDateTimeParserTest.java
@@ -15,22 +15,6 @@ public class SqlRuDateTimeParserTest {
     }
 
     @Test
-    public void whenToDayDate() {
-        String date = "сегодня, 22:29";
-        LocalDateTime localDateTime = LocalDateTime.of(2021, 9, 28, 22, 29);
-        SqlRuDateTimeParser sql = new SqlRuDateTimeParser();
-        assertTrue(localDateTime.isEqual(sql.parse(date)));
-    }
-
-    @Test
-    public void whenToYesterdayDate() {
-        String date = "вчера, 22:29";
-        LocalDateTime localDateTime = LocalDateTime.of(2021, 9, 27, 22, 29);
-        SqlRuDateTimeParser sql = new SqlRuDateTimeParser();
-        assertTrue(localDateTime.isEqual(sql.parse(date)));
-    }
-
-    @Test
     public void whenFalseDate() {
         String date = "3 мар 18, 13:09";
         LocalDateTime localDateTime = LocalDateTime.of(2018, 3, 3, 13, 29);
