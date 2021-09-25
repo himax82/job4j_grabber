9e18527c2a08c2b290ccdce92c96f73deedc5064
himax82 <himax82@gmail.com>
2021-09-25 14:51:43 +0800

    7. Web. [#289478]

--- src/main/java/grabber/Grabber.java
+++ src/main/java/grabber/Grabber.java
@@ -7,6 +7,8 @@ import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
 
 import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.Properties;
@@ -33,6 +35,29 @@ public class Grabber implements Grab {
                 .getResourceAsStream("app.properties")) {
             cfg.load(in);
         }
+    }
+
+    public void web(Store store) {
+        new Thread(() -> {
+            try (ServerSocket server = new ServerSocket(Integer.parseInt(cfg.getProperty("port")))) {
+                while (!server.isClosed()) {
+                    Socket socket = server.accept();
+                    try (OutputStream out = socket.getOutputStream()) {
+                        out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                        for (Post post : store.getAll()) {
+
+                            out.write
+                                    (post.toString().getBytes());
+                            out.write(System.lineSeparator().getBytes());
+                        }
+                    } catch (IOException io) {
+                        io.printStackTrace();
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }).start();
     }
 
     @Override
@@ -79,5 +104,6 @@ public class Grabber implements Grab {
         Scheduler scheduler = grab.scheduler();
         Store store = grab.store();
         grab.init(new SqlRuParse(new SqlRuDateTimeParser()), store, scheduler);
+        grab.web(store);
     }
 }
