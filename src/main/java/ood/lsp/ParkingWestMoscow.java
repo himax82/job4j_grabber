28b10e674e8676e0adf6cb290e96da1e565a4e37
himax82 <himax82@gmail.com>
2021-10-10 21:32:11 +0800

    2. Парковка машин [#853 #214352]

--- src/main/java/ood/lsp/ParkingWestMoscow.java
+++ src/main/java/ood/lsp/ParkingWestMoscow.java
@@ -1,9 +1,39 @@
 package ood.lsp;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ParkingWestMoscow implements Parking {
+
+    private final int lengthSmall;
+    private final int lengthBig;
+
+    private final List<Car> smallCar = new ArrayList<>();
+    private final List<Car> bigCar = new ArrayList<>();
+
+    public ParkingWestMoscow(int lengthSmall, int lengthBig) {
+        this.lengthSmall = lengthSmall;
+        this.lengthBig = lengthBig;
+    }
 
     @Override
     public boolean parkingCar(Car car) {
+        if (car.getSize() == 1) {
+            if (smallCar.size() < lengthSmall) {
+                smallCar.add(car);
+                return true;
+            }
+        } else if (car.getSize() > 1) {
+            if (bigCar.size() < lengthBig) {
+                bigCar.add(car);
+                return true;
+            } else if ((lengthSmall - smallCar.size()) > Math.ceil(car.getSize())) {
+                for (int i = 0; i < (int) Math.ceil(car.getSize()); i++) {
+                    smallCar.add(car);
+                }
+                return true;
+            }
+        }
         return false;
     }
 }
--- test/java/ood/lsp/ParkingTest.java
+++ test/java/ood/lsp/ParkingTest.java
@@ -8,7 +8,6 @@ import static org.junit.Assert.*;
 
 public class ParkingTest {
 
-    @Ignore
     @Test
     public void parckingTrue() {
         Parking parking = new ParkingWestMoscow(2, 2);
@@ -20,15 +19,16 @@ public class ParkingTest {
         assertTrue(parking.parkingCar(car3));
     }
 
-    @Ignore
     @Test
     public void parckingFalse() {
         Parking parking = new ParkingWestMoscow(2, 2);
         Car car1 = new Sedan();
         Car car2 = new Truck(2);
         Car car3 = new Truck(2);
+        Car car4 = new Truck(2);
         parking.parkingCar(car1);
         parking.parkingCar(car2);
-        assertFalse(parking.parkingCar(car3));
+        parking.parkingCar(car3);
+        assertFalse(parking.parkingCar(car4));
     }
 }
