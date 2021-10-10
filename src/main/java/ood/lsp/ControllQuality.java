f3daf7507a017107c023862adc685e7ccef54d26
himax82 <himax82@gmail.com>
2021-10-10 19:56:35 +0800

    1. Хранилище продуктов [#852 #214307]

--- src/main/java/ood/lsp/ControllQuality.java
+++ src/main/java/ood/lsp/ControllQuality.java
@@ -5,7 +5,11 @@ import java.util.List;
 
 public class ControllQuality {
 
-   private List<Storage> list = List.of(new Warehouse(), new Shop(), new Trash());
+   private List<Storage> list;
+
+    public ControllQuality(List<Storage> list) {
+        this.list = list;
+    }
 
     public void put(Products products) {
         for (Storage s : list) {
@@ -25,7 +29,7 @@ public class ControllQuality {
         LocalDate localDate = LocalDate.of(2021, 10, 25);
         LocalDate localDate1 = LocalDate.of(2021, 9, 23);
         LocalDate localDate2 = LocalDate.of(2021, 10, 10);
-        ControllQuality controllQuality = new ControllQuality();
+        ControllQuality controllQuality = new ControllQuality(List.of(new Warehouse(), new Trash(), new Shop()));
         Products products1 = new Fish("Окунь", localDate, localDate1, 50, 0.3);
         controllQuality.put(products1);
         Products products2 = new Food("Беляш", localDate2, localDate1, 40, 0.75);
